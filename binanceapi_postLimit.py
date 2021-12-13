5c8e76c6a13143534b5fe8b6a6c89008b9fc15bb
Laith EL MERSHATI <laith.elmershati@gmail.com>
2021-12-13 18:53:01 +0100

    Signature and Post Limit orders on Binance

---
+++ binanceapi_postLimit.py
@@ -0,0 +1,54 @@
+import time, hmac, requests, json
+
+API_KEY = ''
+API_SECRET = ''
+BURL = 'https://api.binance.com'
+
+endpoint = '/api/v3/order'
+
+
+def get_signed_url(dataQueryString, secret_key):
+    signature = hmac.new(API_SECRET.encode(), dataQueryString.encode(), 'sha256').hexdigest()
+    return (BURL + endpoint + '?' + dataQueryString + '&signature=' + signature)
+
+
+def createOrder(api_key, secret_key, direction, price, amount, pair='BTCUSDT', orderType='LimitOrder'):
+    type = ''
+    if orderType == 'LimitOrder':
+        type = 'LIMIT'
+    
+    # dataQueryString = 'symbol=BTCUSDT&side=BUY&type=LIMIT&timeInForce=GTC&quantity=0.0015&price=30000&recvWindow=20000&timestamp=' + str(int(time.time()*1000))
+    dataQueryString = 'symbol=' + pair +'&side=' + direction.upper() + '&type=' + type + '&timeInForce=GTC&quantity=' + str(amount) + '&price=' + str(price) + '&recvWindow=20000&timestamp=' + str(int(time.time()*1000))
+    
+    headers = {'X-MBX-APIKEY': api_key}
+    url = get_signed_url(dataQueryString, secret_key)
+
+    response = requests.post(url, headers=headers)
+    r = json.loads(response.text)
+
+    print(r['orderId'])
+    return r
+
+
+def cancelOrder(api_key, secret_key, uuid, pair='BTCUSDT'):
+    
+    # dataQueryString = 'symbol=BTCUSDT&orderId=8617530103&recvWindow=20000&timestamp=' + str(int(time.time()*1000))
+    dataQueryString = 'symbol=' + pair + '&orderId=' + str(uuid) + '&recvWindow=20000&timestamp=' + str(int(time.time()*1000))
+    
+    headers = {'X-MBX-APIKEY': api_key}
+    url = get_signed_url(dataQueryString, secret_key)
+
+    response = requests.delete(url, headers=headers)
+    r = json.loads(response.text)
+
+    return r
+
+
+
+# r = createOrder(API_KEY, API_SECRET, 'buy', 30000, 0.0015)
+
+# uuid = 8618063338
+# r = cancelOrder(API_KEY, API_SECRET, uuid)
+
+# print(r)
+
